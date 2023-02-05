70601edecf76dca582696474fc437f6f7385869d
Dennis Syhre <dennis.syhre@gmail.com>
2023-02-05 15:33:13 +0100

    Change build.outDir for app to public/app/

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,4 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), basicSsl()],
+  build: {
+    outDir: '../public/app/'
+  } 
 })
