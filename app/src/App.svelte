36e60da8325dc4e3a418ed4137f569f771afdbca
Dennis Syhre <dennis.syhre@gmail.com>
2023-02-12 12:43:32 +0100

    Add some styles

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -8,20 +8,12 @@
   </div>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+<style lang="scss">
+  main {
+    width: 100%;
+    padding: 100px 50px;
+    @media print {
+      padding: 1cm;
+    }
   }
 </style>
