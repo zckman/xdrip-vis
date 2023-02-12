6e92f805cddbe138a24396d9b5ed6058d15b77e3
Dennis Syhre <dennis.syhre@gmail.com>
2023-02-12 12:39:51 +0100

    Remove demo markup

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,30 +1,11 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-    import Visualization from './lib/Visualization.svelte';
+  import Visualization from './lib/Visualization.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
   <div class="card">
     <Visualization />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
